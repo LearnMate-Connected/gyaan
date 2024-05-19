d4bc6ef8a1b155a7baea6faed8ca66965fba4a64
ABHISHEK KUMAR PANDEY <abhishekpandey2432001@gmail.com>
2024-05-20 00:03:46 +0530

    Minor fixes

--- gyaan/gypermission/admin.py
+++ gyaan/gypermission/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from gypermission.models.base import Subscription, UserSubscription
 
 # Register your models here.
--- gyaan/gyproduct/apps.py
+++ gyaan/gyproduct/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class GyproductConfig(AppConfig):
     name = 'gyproduct'
+    verbose_name = 'Product Management'
